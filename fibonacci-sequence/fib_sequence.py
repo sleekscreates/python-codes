92857ffbfafb6572e09586f6a404ef4f90785db5
sleeks.creates.dev <sleek1.creations@yahoo.com>
2023-04-03 13:50:03 -0400

    initial commit

--- fibonacci-sequence/fib_sequence.py
+++ fibonacci-sequence/fib_sequence.py
@@ -1,1 +1,23 @@
+ # Declare function for an  nth fibonacci
+def Fibonacci(n):
 
+ # Checks whether input is less than 0 then
+   # Prints Invalid input
+    if n < 0:
+        print("Invalid input")
+
+    # checks if n = 0
+    # then it returns 0
+    elif n == 0:
+        return 0 
+ 
+    # checks whether n is 1,2
+    # then it returns 1,2 
+    elif n == 1 or n == 2:
+        return 1 
+ 
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+        
+
+print(Fibonacci(7)) 
--- prime-numbers/prime_numbers.py
+++ prime-numbers/prime_numbers.py
@@ -1,1 +1,19 @@
+#taking input from the user
+num = int(input("Enter any number: "))
+
+#if number entered is less than or equal to 1
+#then the print is not a prime number
+if num <= 1 :
+       print(num, "Is Not a Prime Number.")
+
+# prime number is always greater than 1
+elif  num> 1 :
+    for x in range(2, num):
+        
+        if(num % x) == 0:
+                print(num, "Is Not a Prime Number.")
+                break
+
+        else:
+                print(num, "Is a Prime Number.")
 
