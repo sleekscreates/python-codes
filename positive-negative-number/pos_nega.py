b021793f4d4976635d0424a32754178d5e0cd983
sleeks.creates.dev <sleek1.creations@yahoo.com>
2023-04-06 05:04:27 -0400

    add: initial commit

--- positive-negative-number/pos_nega.py
+++ positive-negative-number/pos_nega.py
@@ -1,1 +1,12 @@
+number = int(input("Enter a number:"))
+if number == 0 :
+    print("This is Zero")
 
+elif number < 0 :
+    print("This Number is a Negative Number")
+
+elif number > 0 :
+    print("This is Number is a Positive Number")
+
+else :
+    print("Incorrect Input, This is Not a Number")
